00d66e3829c0c89da913e5832583d00b394ab4c8
Takiguchii <alexandre.pupim360@gmail.com>
2025-07-31 19:59:16 -0300

    Fix: fixing bungs in 'Appvue'

--- PokedexVueJs/src/App.vue
+++ PokedexVueJs/src/App.vue
@@ -6,9 +6,9 @@ import Footer from './components/FooterComponent.vue'
 <template>
   <div class="flex flex-col min-h-screen bg-gray-100">
     <Navbar />
-    <div class="main container mx-auto px-4 py-6">
+    <div class="main container mx-auto px-4 py-6 flex-1">
       <router-view></router-view>
     </div>
-    <Footer />
+    <Footer class="mt-auto" />
   </div>
 </template>
--- PokedexVueJs/src/components/PokemonCardComponent.vue
+++ PokedexVueJs/src/components/PokemonCardComponent.vue
@@ -8,8 +8,6 @@
 
 <script setup>
     defineProps({
-        pokemon: Object
-        
-        
+        pokemon: Object      
     })
 </script>
