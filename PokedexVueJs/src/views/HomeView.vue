6f08e0897322ca2bafd004f9d6fb4ad44e1a27f7
Takiguchii <alexandre.pupim360@gmail.com>
2025-07-31 21:20:31 -0300

    Feat: Create HomePage

--- PokedexVueJs/src/views/HomeView.vue
+++ PokedexVueJs/src/views/HomeView.vue
@@ -2,7 +2,10 @@
 </script>
 
 <template>
-  <main>
-    <h1>Home</h1>
+  <main class="flex items-center justify-center min-h-screen bg-gray-50">
+    <div class="text-center">
+      <h1 class="text-5xl font-extrabold text-indigo-700 mb-2">PokeBase</h1>
+      <p class="text-lg text-gray-500 mt-2">Aprenda mais sobre seus pokemons</p>
+    </div>
   </main>
 </template>
