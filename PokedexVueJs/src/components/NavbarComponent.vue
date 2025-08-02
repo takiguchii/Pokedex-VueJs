a041390d529f3d96ca20847b904df42ea399872d
Takiguchii <alexandre.pupim360@gmail.com>
2025-08-01 22:16:53 -0300

    feat: Creating a spider graph with pokemon information

--- PokedexVueJs/src/components/NavbarComponent.vue
+++ PokedexVueJs/src/components/NavbarComponent.vue
@@ -1,11 +1,13 @@
 <template>
     <nav class="bg-blue-600 text-white py-4 px-8 flex justify-between items-center">
-        <h1 class="text-lg font-bold">Pokedex</h1>
+        <router-link to="/">
+            <button class="text-lg font-bold focus:outline-none">Pokedex</button>
+        </router-link>
         <ul class="flex gap-6">
             <li>
                 <router-link to="/pokedex" class="hover:underline">Pokedex</router-link>
             </li>
-                       <li>
+            <li>
                 <router-link to="/analise" class="hover:underline">Analise</router-link>
             </li>
         </ul>
---
+++ PokedexVueJs/src/components/PokemonRadarChart.vue
@@ -0,0 +1,36 @@
+<template>
+  <RadarChart :data="chartData" />
+</template>
+
+<script>
+import { RadarChart } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Filler,
+  Legend,
+  LineElement,
+  PointElement,
+  RadialLinearScale,
+  Tooltip
+} from 'chart.js'
+
+ChartJS.register(
+  Filler,
+  Legend,
+  LineElement,
+  PointElement,
+  RadialLinearScale,
+  Tooltip
+)
+
+export default {
+  name: 'PokemonRadarChart',
+  components: { RadarChart },
+  props: {
+    chartData: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
